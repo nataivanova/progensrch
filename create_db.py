4b0f14858bcead83ea43b7524fa9bf42562f1fe6
Lev Eche <projects+catchall@semyon.org>
2022-05-20 14:38:12 -0600

    add inices in create_db

--- create_db.py
+++ create_db.py
@@ -2,8 +2,14 @@
 
 import pickle
 import numpy as np
-import sqlite3
+import sqlite3 as sql
 
+con = sql.connect('/var/tmp/progen_tool/db.sqlite')
+
+cur = con.cursor()
+
+with open('/tmp/alldbfiles','r') as f:
+    allfiles = f.readlines()
 
 cur.execute("drop table if exists tracks")
 cur.execute('''CREATE TABLE tracks (mdonor real, maccretor real, mtrate real, period real, teff real, age real, radius real, dt real, srcfile text)''')
@@ -25,3 +31,18 @@ for fname in allfiles:
         _.append(fname.strip())
         cur.execute("insert into tracks values (?,?,?,?,?,?,?,?,?)", _)
         con.commit()
+
+cur.execute(
+    "CREATE TABLE meta AS
+    SELECT CASE WHEN srcfile LIKE '%/ns_data/%' THEN FALSE ELSE TRUE END as isbh
+    , min(mdonor) as mdonor_min
+    , max(mdonor) as mdonor_max
+    , min(maccretor) as maccretor_min
+    , max(maccretor) as maccretor_max
+    , srcfile FROM tracks group by srcfile"
+)
+cur.execute("create index if not exists idx_meta_srcfile       ON meta(srcfile)")
+cur.execute("create index if not exists idx_meta_maccretor_max ON meta(maccretor_max)")
+cur.execute("create index if not exists idx_meta_maccretor_min ON meta(maccretor_min)")
+cur.execute("create index if not exists idx_meta_donor_max     ON meta(donor_max)")
+cur.execute("create index if not exists idx_meta_donor_min     ON meta(donor_min)")
