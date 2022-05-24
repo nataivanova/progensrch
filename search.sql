4b77060515413c2784edb28116da6b798d886d67
Lev Eche <projects+catchall@semyon.org>
2022-05-24 17:42:49 -0600

    also search on MT_rate in the SQL query

--- search.sql
+++ search.sql
@@ -2,5 +2,6 @@ SELECT *
 FROM tracks
 WHERE mdonor    >= m1_lower   AND mdonor    <= m1_upper
 AND   maccretor >= m2_lower   AND maccretor <= m2_upper
+AND   mtrate    >= mt_lower   AND mtrate    <= mt_upper
 AND   period    >= p_lower    AND period    <= p_upper
 AND   teff      >= teff_lower AND teff      <= teff_upper;
