a5271cf6bc3be8492a95c4838fd1781cdf8c36c6
Lev Eche <projects+catchall@semyon.org>
2022-05-26 15:01:47 -0600

    progenitor database interface class

--- progenlib.py
+++ progenlib.py
@@ -74,14 +74,135 @@ class ProgenitorQuery:
         else:
             raise QueryParametersException('input file does not match template\n' + self.content)
 
+        self.validate()
+
         return None
+
+    def __str__(self) -> None:
+        return str(self.query)
+
+    def validate(self):
+        """ Rudimentary input checks """
+        errors = []
+        if (query['m1'][0] < 0.0 or query['m1'][0] > query['m1'][1]):
+            errors.append("Wrong donor mass range")
+        if (query['m2'][0] < 0.0 or query['m2'][0] > query['m2'][1]):
+            errors.append("Wrong BH mass range")
+        if (query['mt'][0] < -50.0 or query['mt'][0] > query['mt'][1]):
+            errors.append("Wrong MT rate")
+        if (query['p'][0] < 0.0 or query['p'][0] > query['p'][1]):
+            errors.append("Wrong orbital period")
+        if query['teff'][0] > query['teff'][1]:
+            errors.append("Wrong Donor Teff")
+        if (errors):
+            raise QueryParametersException(errors)
+
+class ProgenitorDatabase:
+    """ Instanciate this class to initialize the interface to the database.
+        In the initial implementation, the database is a collection of flat files.
+        The constructor reads in all avaliable paths and infers the start/end masses
+        for donor accretor for each file. Since donors only lose and accreators only
+        gain mass, the mapping file path -> [mass ranges] works as a pair of indices
+        on the two mass parameters.
+    """
+
+    logger = logging.getLogger ('progentool')
+
+    db = {}
+
+    def __init__(self, db_location):
+        """ Read the file system structure and set up the mapping
+            file name -> mass ranges
+        """
+
+        self.logger.info("initializing the database")
+
+        try:
+            os.stat(db_location)
+            for root, dirs, files in os.walk(db_location, followlinks=True):
+                logger.debug ('walking the file system. We are at ' + root
+                              + 'and see dirs '    + str(dirs)
+                              + 'as well as files' + str(files) )
+
+                for file in files:
+                    fullpath = root + '/' + file
+                    logger.debug('pondering over file ' + fullpath)
+
+                    f = re.match(  r"""m_(?P<m1>\d+\.?\d+)_p_(?P<p>-?\d+\.?\d+)\.data$"""
+                                   , file
+                                   , flags = re.X)
+                    g = re.match(  r"""
+                    /.*
+                    / (?P<isbh>[01])?        # 0 = NS, 1 = BH
+                    /(?P<m2>[^/]*)?          # for black holes, the directory structure is split by accretor mass
+                    /[ls]m[ls]p"""
+                                   , root
+                                   , flags = re.X)
+
+                    if f:
+                        if not g:
+                            raise ProgenDBInitException('found a data file but unable to determine the system type')
+
+                        self.logger.debug('adding file ' + file + ' to the database')
+                        self.db[fullpath] = { 'isbh':  int(g.group('isbh'))
+                                            , 'm1' : float(f.group('m1'))
+                                            , 'p'  : float(f.group('p')) }
+                        if self.db[fullpath]['isbh']:
+                            self.db[fullpath]['m2'] = float(g.group('m2'))
+
+
+        except Exception:
+            raise ProgenDBInitException('cannot initialize the database')
+
+
+        self.logger.info('finished building the database, learned about ' + str(len(self.db)) + ' files')
+
+    def __str__(self) -> None:
+        return str(self.db)
+
+
+    def view(self, query: ProgenitorQuery) -> dict:
+        """
+        Given a query, restrict the view of candidate data files to the ones within the mass and period range
+        """
+        # The 3.5 is present there because the maximum mass that can be accreted by the black hole is 0.5*7.0 (efficiency*max donor mass) = 3.5
+        maxtransferredmass = 7.0 * 0.5
+
+        self.logger.info('creating a view for query: ' + str(query) )
+
+        v = filter (  lambda _: _[1]['m1'] >= query['m1'][0]
+                      and       _[1]['isbh'] == query['bhns']
+                      and ( True if not _[1]['isbh']
+                            else (     _[1]['m2'] < query['m2'][1]
+                                   and _[1]['m2'] > query['m2'][0] - maxtransferredmass ) )
+                      , self.db.items() )
+
+        self.logger.info('built view with ' + str(len(dict(x))) + ' candidate data files' )
+
+        return v
+
+        """
+        self.paths = filter (lambda x: os.path.exists(x)
+             , [ db_location
+                 + str(self.query['bhns'])    + '/'
+                 + str(accretor_mass)         + '/'
+                 + str(quadrant)              + '/'
+                 + 'm_'  + f'{donor_mass:4.2f}'
+                 + '_p_' + f'{donor_mass:4.2f}'
+                 + '.data'
+          for accretor_mass in self.accretor_mass_partition()
+          for quadrant      in self.confs.keys()
+          for donor_mass    in self.confs[quadrant][0] if donor_mass >= self.query['m1'][0]
+          for period        in self.confs[quadrant][1]
+          ])
+       """
 
 
 class ProgenitorSearcher:
 
     query = {}
     confs = {}
-    db_location = '/tmp/'
+    db_location = '/srv/progen_tool'
     progens = []
     errors  = []
 
@@ -167,7 +288,7 @@ class ProgenitorSearcher:
         while (start <= end):
             step = step+1
             mid = (start + end) // 2
-            
+
             if mid == len(array):
                 return len(array)-1
 
@@ -183,57 +304,119 @@ class ProgenitorSearcher:
 
     # Function to find index where system starts MT
     def find_mt_start(find, mt_arr):
-        
+
         idx_start = 0
 
         if len(mt_arr) <= 5:
             return idx_start
 
         for i in range(len(mt_arr)-3):
-            if (mt_arr[i] >= -15 and mt_arr[i+1] >= -15 and mt_arr[i+2] >= -15 and mt_arr[i+3] >= -15):
+            if ( mt_arr[i] >= -15 and mt_arr[i+1] >= -15 and mt_arr[i+2] >= -15 and mt_arr[i+3] >= -15):
                 idx_start = i
                 break
 
         return idx_start
 
-    def accretor_mass_partition(self):
-        # hese lines are for choosing which accretor masses will be relevant for the search.
-        # For example, if the query given is 9.0,9.5 for accretor mass, the code will not go into the 10 msol database at all, since there will be no matches there.
-        # The second condition for choosing the relevant accretor uses the 3.5.
-        # So for our example, if we had the 5 msol database included, the code would check whether 5+3.5=8.5 is less than the lower limit of the query (9.0).
-        # If that is the case, it will not search in the 5 msol database.
-        # The 3.5 is present there because the maximum mass that can be accreted by the black hole is 0.5*7.0 (efficiency*max donor mass) = 3.5
-        # So no black hole in the 5 msol database will reach the mass given in the query, so there is no need to search through 5 msol.
-        # Therefore, for our example, only 7msol  database will be selected to be searched through.
 
-        print(self.query)
-        if self.query['bhns']:
-            return  [ x for x in self.bh_masses if ( x < self.query['m2'][1] ) and ( x > self.query['m2'][0] - 3.5 ) ]
-        else:
-            return ['/']
+    def get_vals(self, filepath) -> dict:
+        with open(filepath, 'rb') as infile:
+            donor_masses, accretor_masses, mt_rates, periods, teffs, ages, radii, dts  = pickle.load(infile)
+            # Ignore simulations where final donor mass is greater than upper limit of donor mass in query.
+            # Similarly, ignore simulations where initial accretor mass is greater than upper limit of accretor mass in query
+            if (     donor_masses[-1]   <= self.query['m1'][1]
+                 and accretor_masses[0] <= self.query['m2'][1] ):
+                return { 'm1': donor_masses
+                         , 'm2': accretor_masses
+                         , 'mt': mt_rates
+                         , 'periods': periods
+                         , 'teffs': teffs
+                         , 'ages': ages
+                         , 'radii': radii
+                         , 'dts': dts }
+            else:
+                return {}
 
-    def gen_search_paths(self):
-        return filter (lambda x: os.path.exists(x)
-             , [ '/srv/progen_tool/'
-                 + str(self.query['bhns'])    + '/'
-                 + str(accretor_mass)         + '/'
-                 + str(quadrant)              + '/'
-                 + 'm_'  + f'{donor_mass:4.2f}'
-                 + '_p_' + f'{donor_mass:4.2f}'
-                 + '.data'
-          for accretor_mass in self.accretor_mass_partition()
-          for quadrant      in self.confs.keys()
-          for donor_mass    in self.confs[quadrant][0] if donor_mass >= self.query['m1'][0]
-          for period        in self.confs[quadrant][1]
-          ]
+    def extract_progens(self, vals) -> None:
+
+        def match_props(data) -> bool:
+            query = self.query
+            if (  data[1] < query['m2'][0]      or data[1] > query['m2'][1]
+                  or data[2] < query['mt'][0]   or data[2] > query['mt'][1]
+                  or data[3] < query['p'][0]    or data[3] > query['p'][1]
+                  or data[4] < query['teff'][0] or data[4] > query['teff'][1]
+                  or data[0] < query['m1'][0]   or data[0] > query['m1'][1]):
+                return False
+            else:
+                return True
+
+        flag, pflag, start_m1, end_m1 = 0, 0, 0, 0
+        start_m1, end_m1 = self.search(vals['m1'], query['m1'][0]),   self.search(vals['m1'], query['m1'][1])
+
+        idx_low = -1
+        idx_high = -1
+        obs_time = 0.0
+        mt_start = 0
+        for k in range(start_m, end_m+1):
+            if match_props( [ vals[_][k] for _ in ['m1', 'm2', 'mt', 'p', 'teff'] ] ):
+                idx_low = k
+            if (   (flag    and idx_low != -1 and idx_high == -1)
+                   or (flag and k == end_m    and idx_high == -1) ):
+                pflag = 1
+                idx_high = k
+                obs_time = obs_time + vals[5][idx_high]-vals[5][idx_low]
+                idx_low = -1
+                idx_high = -1
+                # If simulated system spends time as the observed system, find start of MT information and add to list of progenitors
+            if pflag:
+                mt_start = self.find_mt_start(vals[2])
+                tot_time = vals[5][-1]-vals[5][0]
+                progens.append([i,j,vals[1][0],obs_time,tot_time,vals[0][mt_start],np.log10(vals[3][mt_start])])
+                print("Found Progenitor -> "+fpath)
+            else:
+                print("No path found: "+fpath)
+
+        self.progens = progens
+        if not progens:
+            raise NoProgensFoundException('No progenitors found')
+
 
     # Function to look through data files to find progenitors for the query
     def do_search(self):
+
+        def match_props(data):
+            query = self.query
+            try:
+                if 'm1' in query:
+                    if (data[0] < query['m1'][0] or data[0] > query['m1'][1]):
+                        #print("No m1")
+                        return 0
+                if 'm2' in query:
+                    if (data[1] < query['m2'][0] or data[1] > query['m2'][1]):
+                        #print("No m2")
+                        return 0
+                if 'mt' in query:
+                    if (data[2] < query['mt'][0] or data[2] > query['mt'][1]):
+                        #print("No mt")
+                        return 0
+                if 'p' in query:
+                    if (data[3] < query['p'][0] or data[3] > query['p'][1]):
+                        #print("No p")
+                        return 0
+                if 'teff' in query:
+                    if (data[4] < query['teff'][0] or data[4] > query['teff'][1]):
+                        #print("No teff")
+                        return 0
+            except Exception as e:
+                errors.append("Error in matching properties")
+                return 0
+
+            return 1
+
         query = self.query
         confs = self.confs
-        
+
         progens = []
-        
+
         data_paths = []
         # Select relevant databases to search through
         if 0 == query['bhns']:
